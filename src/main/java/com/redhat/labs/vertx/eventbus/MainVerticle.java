b49a8fb73910bdacae897bae99b9f8f0fb3c6217
Deven Phillips <deven.phillips@redhat.com>
2018-04-29 11:03:29 -0400

    Revert to correct version of MainVerticle

--- src/main/java/com/redhat/labs/vertx/eventbus/MainVerticle.java
+++ src/main/java/com/redhat/labs/vertx/eventbus/MainVerticle.java
@@ -33,55 +33,10 @@ import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-/**
- * An example Verticle which demonstrates 2 of the Vert.x EventBus bridge implementations for both Apache Camel and
- * WebSockets.
- */
 public class MainVerticle extends AbstractVerticle {
 
     private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
     public static final String HTTP_URL_REGEX = "(https?://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|])";
-
-    /**
-     * A simple implementation of {@link RouteBuilder} which attaches to Twitter's streaming API
-     */
-    private class TweetStreamer extends RouteBuilder {
-
-        static final String TWITTER_URI = "twitter://streaming/filter?type=event&lang=en&keywords=";
-
-        @Override
-        public void configure() throws Exception {
-            // Create a list of keywords we would like to watch for on Twitter.
-            List<String> keywords = new ArrayList<>();
-            keywords.add("#openinnovationlabs");
-            keywords.add("#RHSummit");
-            keywords.add("#RedHat");
-            keywords.add("#Vertx");
-            keywords.add("#infinispan");
-            keywords.add("#openshift");
-            keywords.add("#reactive");
-            keywords.add("#openshift");
-            JsonObject config = vertx.getOrCreateContext().config();
-
-            TwitterComponent tc = getContext().getComponent("twitter", TwitterComponent.class);
-            tc.setAccessToken(config.getString("twitter_access_token"));
-            tc.setAccessTokenSecret(config.getString("twitter_access_secret"));
-            tc.setConsumerKey(config.getString("twitter_consumer_key"));
-            tc.setConsumerSecret(config.getString("twitter_consumer_secret"));
-            LOG.debug(config.encodePrettily());
-
-            // stream twitter search for new tweets
-            String keywordList = keywords.stream().collect(Collectors.joining(","));
-            String uri = TWITTER_URI + URLEncoder.encode(keywordList, "utf8");
-
-            from(uri)
-                    .filter(body().isNotNull())
-                    .filter(body().isInstanceOf(Status.class))
-                    .filter(simple("${body.retweet} == false"))
-                    .transform(simple("${body.text}"))
-                    .to("direct:tweets");
-        }
-    }
 
     @Override
     public void start(io.vertx.core.Future<Void> startFuture) {
@@ -128,7 +83,6 @@ public class MainVerticle extends AbstractVerticle {
         router.route("/api/v1/tweets/recent").handler(this::handleRecentTweets);
 
         // Static content handler pointing to the "webroot" contained in src/main/resources
-        // Handles all other HTTP requests received and handles sending 404 if items are not found.
         router.route("/*").handler(StaticHandler.create("webroot").setCachingEnabled(false));
         return vertx.createHttpServer().requestHandler(router::accept).rxListen(8080).toCompletable();
     }
