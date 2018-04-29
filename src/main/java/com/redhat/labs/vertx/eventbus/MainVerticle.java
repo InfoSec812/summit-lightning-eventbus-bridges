39f22f72d2069695c506aebee49bab4177d2969a
Deven Phillips <deven.phillips@redhat.com>
2018-04-28 21:22:56 -0400

    Added Rx Twitter client

--- src/main/java/com/redhat/labs/vertx/eventbus/MainVerticle.java
+++ src/main/java/com/redhat/labs/vertx/eventbus/MainVerticle.java
@@ -1,25 +1,22 @@
 package com.redhat.labs.vertx.eventbus;
 
-import io.vertx.camel.CamelBridge;
-import io.vertx.camel.CamelBridgeOptions;
-import io.vertx.camel.InboundMapping;
-import io.vertx.core.AbstractVerticle;
+import com.shekhargulati.reactivex.twitter.TweetStream;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.bridge.PermittedOptions;
 import io.vertx.ext.web.handler.sockjs.BridgeOptions;
-import io.vertx.ext.web.handler.sockjs.PermittedOptions;
-import io.vertx.ext.web.handler.sockjs.SockJSHandler;
-import org.apache.camel.CamelContext;
-import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.twitter.TwitterComponent;
-import org.apache.camel.impl.DefaultCamelContext;
+import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.core.eventbus.EventBus;
+import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.RoutingContext;
+import io.vertx.reactivex.ext.web.handler.StaticHandler;
+import io.vertx.reactivex.ext.web.handler.sockjs.SockJSHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import twitter4j.Status;
+import twitter4j.conf.ConfigurationBuilder;
 
-import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,52 +26,10 @@ public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start() {
-        List<String> keywords = new ArrayList<>();
-        keywords.add("#openinnovationlabs");
-        keywords.add("#RHSummit");
-        keywords.add("#RedHat");
-        keywords.add("#Vertx");
-        keywords.add("#infinispan");
-        keywords.add("#openshift");
-        keywords.add("#reactive");
-        keywords.add("#openshift");
-            CamelContext camel = new DefaultCamelContext();
-
-        try {
-            camel.addRoutes(new RouteBuilder() {
-                @Override
-                public void configure() throws Exception {
-                    JsonObject config = vertx.getOrCreateContext().config();
-
-                    TwitterComponent tc = getContext().getComponent("twitter", TwitterComponent.class);
-                    tc.setAccessToken(config.getString("twitter_access_token"));
-                    tc.setAccessTokenSecret(config.getString("twitter_access_secret"));
-                    tc.setConsumerKey(config.getString("twitter_consumer_key"));
-                    tc.setConsumerSecret(config.getString("twitter_consumer_secret"));
-                    LOG.debug(config.encodePrettily());
-
-                    // stream twitter search for new tweets
-                    String uri = "twitter://streaming/filter?type=event&lang=en&keywords=" + URLEncoder.encode(keywords.stream().collect(Collectors.joining(",")), "utf8");
-
-                    from(uri)
-                            .filter(body().isNotNull())
-                            .filter(body().isInstanceOf(Status.class))
-                            .filter(simple("${body.retweet} == false"))
-                            .transform(simple("${body.text}"))
-                            .to("direct:tweets");
-                }
-            });
-            camel.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        CamelBridge.create(vertx, new CamelBridgeOptions(camel).addInboundMapping(InboundMapping
-                                        .fromCamel("direct:tweets").usePublish().toVertx("tweet.stream"))).start();
-
         Router router = Router.router(vertx);
 
         BridgeOptions options = new BridgeOptions();
-        options.addOutboundPermitted(new PermittedOptions().setAddress("tweet.stream"));
+        options.setOutboundPermitted(Arrays.asList(new PermittedOptions().setAddress("tweet.status")));
         SockJSHandler sockJSHandler = SockJSHandler.create(vertx).bridge(options);
 
         router.route("/eventbus/*").handler(sockJSHandler);
@@ -84,7 +39,36 @@ public class MainVerticle extends AbstractVerticle {
 
         vertx.createHttpServer().requestHandler(router::accept).listen(8080);
 
-        vertx.eventBus().consumer("tweet.stream").handler(m -> LOG.info(m.body().toString()));
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        JsonObject config = vertx.getOrCreateContext().config();
+        cb.setDebugEnabled(true)
+            .setOAuthConsumerKey(config.getString("twitter_consumer_key"))
+            .setOAuthConsumerSecret(config.getString("twitter_consumer_secret"))
+            .setOAuthAccessToken(config.getString("twitter_access_token"))
+            .setOAuthAccessTokenSecret(config.getString("twitter_access_secret"));
+        EventBus eb = vertx.eventBus();
+
+        String[] keywords = config.getJsonArray("keywords").getList()
+                                    .stream().map(Object::toString)
+                                    .collect(Collectors.joining(","))
+                                    .toString()
+                                    .split(",");
+        TweetStream.of(cb.build(), keywords)
+                .map(this::mapToJsonObject)
+                .doOnNext(j -> LOG.info(j.encodePrettily()))
+                .subscribe(j -> eb.send("tweet.status", j));
+    }
+
+    private JsonObject mapToJsonObject(Status s) {
+        return new JsonObject()
+                        .put("body", s.getText())
+                        .put("id", s.getId())
+                        .put("url", String.format("https://twitter.com/%s/status/%d", s.getUser().getScreenName(), s.getId()))
+                        .put("user", new JsonObject()
+                            .put("handle", s.getUser().getScreenName())
+                            .put("img", s.getUser().getMiniProfileImageURL())
+                            .put("url", s.getUser().getURL())
+                        );
     }
 
 }
